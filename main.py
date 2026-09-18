7a2cb3eefce22c67bf0572217d10eab23df00125
hurrutiaca26aabd <hurrutiaca26aabd@ikzubirimanteo.com>
2026-09-18 10:12:07 +0200

    asmakizuna egina

--- main.py
+++ main.py
@@ -10,6 +10,44 @@ bolas.insert(posicion_aleatoria, bola.Bola(1.1))  # Bola más pesada
 
 balanza = balanza.Balanza()
 
-print(balanza.pesar(bolas[:4], bolas[4:8]))  # Compara las primeras 4 bolas con las últimas 4
+bolaPisutxuena = 0
 
-print(balanza.emaitza(bolas[4]))  # Muestra el resultado de la comparación
+i = 0
+
+emaitza = balanza.pesar(bolas[:3], bolas[3:6])
+
+if emaitza == 1:
+    emaitza = balanza.pesar([bolas[0]], [bolas[1]])
+
+    if emaitza == 1:
+        bolaPisutxuena = 0
+    elif emaitza == -1:
+        bolaPisutxuena = 1
+    else:
+        bolaPisutxuena = 2
+
+elif emaitza == -1:
+    emaitza = balanza.pesar([bolas[3]], [bolas[4]])
+
+    if emaitza == 1:
+        bolaPisutxuena = 3
+    elif emaitza == -1:
+        bolaPisutxuena = 4
+    else:
+        bolaPisutxuena = 5
+
+else:
+    emaitza = balanza.pesar([bolas[6]], [bolas[7]])
+    
+    if emaitza == 1:
+        bolaPisutxuena = 6
+    elif emaitza == -1:
+        bolaPisutxuena = 7
+    else:
+        bolaPisutxuena = 8
+
+
+print(balanza.emaitza(bolas[bolaPisutxuena]))
+
+#print(balanza.pesar(bolas[:4], bolas[4:8]))  # Compara las primeras 4 bolas con las últimas 4
+#print(balanza.emaitza(bolas[4]))  # Muestra el resultado de la comparación
