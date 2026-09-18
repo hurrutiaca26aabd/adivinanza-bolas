997d4753b1003f263faee2df7656c3696ab1ebe4
hurrutiaca26aabd <hurrutiaca26aabd@ikzubirimanteo.com>
2026-09-18 09:08:40 +0200

    hegoi 1

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import bola
 import random
 import balanza
 
+# Hegoiren ariketa
+
 bolas = [bola.Bola(1) for _ in range(8)]
 posicion_aleatoria = random.randint(0,8)
 bolas.insert(posicion_aleatoria, bola.Bola(1.1))  # Bola más pesada
